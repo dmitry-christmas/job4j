9453ad8d39d8ffe9cf735237e7f327130fa14c67
dmitry-christmas <mailto:dmr1433@gmail.com>
2018-08-30 13:54:56 +0200

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,12 +6,12 @@ package ru.job4j.calculate;
 * @version $Id$
 * @since 0.1
 */
-public class Calculate{
+public class Calculate {
 	/**
 	* Main.
 	* @param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
 	/**
@@ -19,7 +19,7 @@ public class Calculate{
 	* @param name Your name.
 	* @return Echo plus your name.
 	*/
-	public String echo(String name){
+	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
 }
