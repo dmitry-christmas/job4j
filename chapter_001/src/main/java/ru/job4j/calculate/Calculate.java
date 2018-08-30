9567f6648c0ef85f301918222b0fdd24eb95114d
dmitry-christmas <mailto:dmr1433@gmail.com>
2018-08-30 10:20:46 +0200

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,14 @@
-package main.java.ru.job4j.calculate;
+package ru.job4j.calculate;
+/**
+* Calculate
+*
+* @author Dmitry Rozhdestvenskiy (mailto:dmr1433@gmail.com)
+*/
 public class Calculate{
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitry Rozhdestvenskiy (mailto:dmr1433@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
