b281fe0f21c06038349e9a7ec29db6790ef4595f
dmitry-christmas <mailto:dmr1433@gmail.com>
2018-09-05 20:32:47 +0200

    5.3. Bulid a chessboard in pseudographics

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+/**
+ * Построение шахматной доски заданной ширины и высоты.
+ *
+ * @author Dmitry Rozhdestvenskiy (mailto:dmr1433@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Board {
+    /**
+     * @param width ширина доски.
+     * @param height высота доски.
+     *
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
