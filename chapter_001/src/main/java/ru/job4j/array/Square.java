0a1ac019822f668d3a777be8a6f375e2bc56d119
dmitry-christmas <mailto:dmr1433@gmail.com>
2018-09-08 16:46:22 +0200

    Checkstyle

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -13,8 +13,8 @@ public class Square {
      */
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
-        for (int i = 0; i != bound; i++){
-            rst[i] = (int) Math.pow(i +1, 2);
+        for (int i = 0; i != bound; i++) {
+            rst[i] = (int) Math.pow(i + 1, 2);
         }
         return rst;
     }
