d30bbae8da3cb21482922abedcb232e980a3e02e
dmitry-christmas <mailto:dmr1433@gmail.com>
2018-09-08 16:24:06 +0200

    6.0. Filling of an Array with the power of number

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+/**
+ * Заполнение массива степенями чисел.
+ *
+ * @author Dmitry Rozhdestvenskiy (mailto:dmr1433@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Square {
+    /**
+     * @param bound .
+     * @return
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 0; i != bound; i++){
+            rst[i] = (int) Math.pow(i +1, 2);
+        }
+        return rst;
+    }
+}
