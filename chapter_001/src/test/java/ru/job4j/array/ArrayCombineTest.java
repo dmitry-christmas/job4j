4c8391aaad19408ddc5a1b1b04791358cc2a6ead
dmitry-christmas <mailto:dmr1433@gmail.com>
2018-10-03 18:33:49 +0200

    7.1. ArrayCombine corrected

--- chapter_001/src/test/java/ru/job4j/array/ArrayCombineTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCombineTest.java
@@ -9,13 +9,39 @@ import static org.junit.Assert.assertThat;
  */
 public class ArrayCombineTest {
     @Test
-    public void when1324675Then1234567() {
+    public void when1234345Then1234567() {
         ArrayCombine arrayCombine = new ArrayCombine();
         int[] array1 = {1, 2, 3, 4};
         int[] array2 = {3, 4, 5};
         int[] result = arrayCombine.combine2Arrays(array1, array2);
         int[] expect = {1, 2, 3, 3, 4, 4, 5};
         assertThat(result, is(expect));
-
+    }
+    @Test
+    public void when1234AndEmptyThen1234() {
+        ArrayCombine arrayCombine = new ArrayCombine();
+        int[] array1 = {1, 2, 3, 4};
+        int[] array2 = {};
+        int[] result = arrayCombine.combine2Arrays(array1, array2);
+        int[] expect = {1, 2, 3, 4};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void when12345And678910Then12345678910() {
+        ArrayCombine arrayCombine = new ArrayCombine();
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {6, 7, 8, 9, 10};
+        int[] result = arrayCombine.combine2Arrays(array1, array2);
+        int[] expect = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void when12And678910Then12678910() {
+        ArrayCombine arrayCombine = new ArrayCombine();
+        int[] array1 = {1, 2};
+        int[] array2 = {6, 7, 8, 9, 10};
+        int[] result = arrayCombine.combine2Arrays(array1, array2);
+        int[] expect = {1, 2, 6, 7, 8, 9, 10};
+        assertThat(result, is(expect));
     }
 }
