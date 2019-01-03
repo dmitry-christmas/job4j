1272e8dc2c7356b18aba392790aff0ecc4a5d571
dmitry-christmas <mailto:dmr1433@gmail.com>
2019-01-03 11:14:53 +0100

    Coffee Machine

--- chapter_002/src/test/java/ru/job4j/coffeemachine/ChangeTest.java
+++ chapter_002/src/test/java/ru/job4j/coffeemachine/ChangeTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.coffeemachine;
 import org.junit.Test;
 import ru.job4j.coffeemachine.coins.*;
+
+import java.util.Arrays;
 import java.util.List;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -11,16 +13,24 @@ public class ChangeTest {
     public void startCash790BuyEspresso16Cash806() {
         Cashier cashier = new Cashier();
         cashier.buy(1, 5, new int[] {20});
-        assertThat(cashier.cashRemain(), is(806));
+        assertThat(cashier.cashRemain(), is(1776));
     }
     @Test
     public void ifchange38Then2010521() {
         Cashier cashier = new Cashier();
         List<Coin> change = cashier.change(70, 32);
+        /*
         StringBuilder result = new StringBuilder();
         for (Coin coin : change) {
             result.append(coin.getValue());
         }
         assertTrue("2010521".equals(result.toString()));
+        */
+        List<Coin> expect = Arrays.asList(new Coin20(),
+                new Coin10(),
+                new Coin5(),
+                new Coin2(),
+                new Coin1());
+        assertThat(change, is(expect));
     }
 }
