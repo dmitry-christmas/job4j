751759f559495462ca1b85fab2097767d07b4f1b
dmitry-christmas <mailto:dmr1433@gmail.com>
2019-01-06 16:36:26 +0100

    ConvertList2Array corrected

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -12,8 +12,6 @@ public class ConvertList2Array {
                     if (counter < list.size()) {
                         array[i][j] = list.get(counter);
                         counter++;
-                    } else {
-                        array[i][j] = 0;
                     }
                 }
             }
