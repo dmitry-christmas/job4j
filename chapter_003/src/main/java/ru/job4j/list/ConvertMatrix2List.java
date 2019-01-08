1de7bcb561ad279fce375c679560348260258327
dmitry-christmas <mailto:dmr1433@gmail.com>
2019-01-08 17:04:55 +0100

    ConvertMatrix

--- chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ConvertMatrix2List {
     public List<Integer> toList(int[][] array) {
         List<Integer> list = new ArrayList<>();
-        for (int i[] : array) {
+        for (int[]i : array) {
             for (int j : i) {
                 list.add(j);
             }
