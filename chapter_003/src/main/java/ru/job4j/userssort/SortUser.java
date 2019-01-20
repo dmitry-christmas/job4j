80ff4cfa17f0f1e1e46b43d3779650ce4803557c
dmitry-christmas <mailto:dmr1433@gmail.com>
2019-01-20 10:51:30 +0100

    Sorting. int Comparable

--- chapter_003/src/main/java/ru/job4j/userssort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/userssort/SortUser.java
@@ -7,14 +7,14 @@ import java.util.TreeSet;
 
 public class SortUser {
     public TreeSet<User> sort(List<User> users) {
-        Comparator<User> comparator = new Comparator<User>() {
+        /*Comparator<User> comparator = new Comparator<User>() {
             @Override
             public int compare(User o1, User o2) {
                 return o1.getAge() - o2.getAge();
             }
-        };
-        TreeSet<User> result = new TreeSet(comparator);
-        users.sort(comparator);
+        };*/
+        TreeSet<User> result = new TreeSet<>();
+        //users.sort();
         result.addAll(users);
         return result;
     }
--- chapter_003/src/main/java/ru/job4j/userssort/User.java
+++ chapter_003/src/main/java/ru/job4j/userssort/User.java
@@ -1,12 +1,15 @@
 package ru.job4j.userssort;
 
-public class User {
+public class User implements Comparable<User>{
     private String name;
     private int age;
 
     public User(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+    public int compareTo(User o) {
+        return age - o.getAge();
     }
 
     public String getName() {
