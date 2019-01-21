2156994c08a71fd196d4be26c0945976d9902876
dmitry-christmas <mailto:dmr1433@gmail.com>
2019-01-21 22:39:27 +0100

    StringCompare

---
+++ chapter_003/src/main/java/ru/job4j/userssort/ListCompare.java
@@ -0,0 +1,27 @@
+package ru.job4j.userssort;
+
+import java.util.Comparator;
+
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        int indexMax = left.length() > right.length() ? right.length() : left.length();
+        for (int i = 0; i < indexMax; i++) {
+            if (left.charAt(i) < right.charAt(i)) {
+                result = -1;
+                break;
+            } else if (left.charAt(i) > right.charAt(i)) {
+                result = 1;
+                break;
+            }
+        }
+        if (result == 0 && left.length() > right.length()) {
+            result = 1;
+        }
+        if (result == 0 && left.length() < right.length()) {
+            result = - 1;
+        }
+        return result;
+    }
+}
